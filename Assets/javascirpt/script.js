5fc731db4a5bba1285487c21e67396ded492909e
Diego Menezes dos Santos <diego@ceh.ce.br>
2023-08-24 13:53:52 -0300

    Add style and almost finish js

--- Assets/javascirpt/script.js
+++ Assets/javascirpt/script.js
@@ -1,31 +1,26 @@
-const userName = document.querySelector('#name')
-const userHeight = document.querySelector('#height')
-const userWeight = document.querySelector('#weight')
-const btnCalc = document.querySelector('#calc')
+const userName = document.querySelector("#name");
+const userHeight = document.querySelector("#height");
+const userWeight = document.querySelector("#weight");
+const btnCalc = document.querySelector("#calc");
+const res = document.querySelector("#awnser");
 
-
-
-
-btnCalc.addEventListener('click', () => {
-    const name = userName.value
-    const height = Number(parseFloat(userHeight.value).toFixed(1));
-    const weight = Number(parseFloat(userWeight.value).toFixed(1));
-    const imc = Number(weight / (height * height)).toFixed(1)
-    console.log(imc)
-    function calcImc (){
-        
-     if (imc < 18.5){
-        console.log('abaixo do peso')
-     } else if (imc < 24.9){
-        console.log('peso ideal')
-     } else if (imc < 29.9){
-        console.log('sobre peso')
-     } else {
-        console.log('gordola')
-     }
-        
+btnCalc.addEventListener("click", () => {
+  const name = userName.value;
+  const height = Number(parseFloat(userHeight.value).toFixed(1));
+  const weight = Number(parseFloat(userWeight.value).toFixed(1));
+  const imc = Number(weight / (height * height)).toFixed(1);
+  res.innerHTML = "";
+  function calcImc() {
+    if (imc < 18.5) {
+      res.innerHTML = `${name} seu IMC é de ${imc} e voce está abaixo do peso`;
+    } else if (imc < 24.9) {
+      res.innerHTML = `${name} seu IMC é de ${imc} e voce está no peso ideal`;
+    } else if (imc < 29.9) {
+      res.innerHTML = `${name} seu IMC é de ${imc} e voce está no acima do peso`;
+    } else {
+      res.innerHTML = `${name} seu IMC é de ${imc} e voce está Obeso`;
     }
+  }
 
-    calcImc()
-})
- 
+  calcImc();
+});
