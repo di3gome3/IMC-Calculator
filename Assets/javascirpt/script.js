43d5d7076ab7cf904403306699707ec082c64992
Diego <santosd630@gmail.com>
2023-08-24 19:55:18 -0300

    clean js

--- Assets/javascirpt/script.js
+++ Assets/javascirpt/script.js
@@ -4,13 +4,14 @@ const userWeight = document.querySelector("#weight");
 const btnCalc = document.querySelector("#calc");
 const res = document.querySelector("#awnser");
 
-btnCalc.addEventListener("click", () => {
+btnCalc.addEventListener("click", () =>{
   const name = userName.value;
-  const height = Number(parseFloat(userHeight.value).toFixed(1));
-  const weight = Number(parseFloat(userWeight.value).toFixed(1));
-  const imc = Number(weight / (height * height)).toFixed(1);
+  const height = parseFloat(userHeight.value);
+  const weight = parseFloat(userWeight.value);
   res.innerHTML = "";
-  function calcImc() {
+
+  if (name !== "" && userHeight !== "" && userWeight !== "") {
+    let imc = Number(weight / (height * height)).toFixed(1);
     if (imc < 18.5) {
       res.innerHTML = `${name} seu IMC é de ${imc} e voce está abaixo do peso`;
     } else if (imc < 24.9) {
@@ -20,7 +21,7 @@ btnCalc.addEventListener("click", () => {
     } else {
       res.innerHTML = `${name} seu IMC é de ${imc} e voce está Obeso`;
     }
+  } else {
+    alert("Preencha todos os dados");
   }
-
-  calcImc();
 });
